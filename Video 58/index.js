b9b66a04a7aa312af998f052166951646d3cafd0
MShuja001 <mshuja8886@gmail.com>
2025-10-21 21:34:16 +0500

    Initial commit

---
+++ Video 58/index.js
@@ -0,0 +1,36 @@
+console.log("Hello, Welcome to Video 58");
+
+// function good(name) {
+//     console.log("Hello " + name + ", Have a good day!");
+//     console.log("Hello " + name + ", Have a great day!");
+//     console.log("Hello " + name + ", Have a brillient day!");
+//     console.log("Hello " + name + ", Have an awesome day!");
+// }
+
+// good("Jhon Doe");
+// const min = (name) => {
+//         console.log("Hello " + name + ", Have a good day!");
+//         console.log("Hello " + name + ", Have a great day!");
+//         console.log("Hello " + name + ", Have a brillient day!");
+//         console.log("Hello " + name + ", Have an awesome day!");
+    
+// }
+// min("Jhon Doe");\
+// const min = (a,b) =>{
+//     console.log("The sum of the numbers are ",(a+b));
+//     return a+b;
+// }
+//  result = min(5,6);
+//  console.log(result);
+// function sum(a,b , c=4){
+//     return a+b+c;
+// }
+// let summ= sum(2,4,3);
+// console.log(summ);
+ const max = (x) =>{
+    console.log("I am an arrow function ",x);
+ }
+    max(23);
+    max("Of Shuja");
+    max(221);
+    
---
+++ video 2/script.js
@@ -0,0 +1,1 @@
+alert("Hello Why are you here?")
