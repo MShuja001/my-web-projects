b8c37125bd2a6d5088a5494b91f1d3e7603aa9d3
Mshuja001 <Mshuja886@gmail.com>
2026-08-21 13:51:31 +0500

    fixed height issue in netflix and added javascript

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,1 +1,12 @@
-document.querySelector(".")
+let hamnav = document.querySelector(".hamnav")
+let img = document.querySelector(".hamburg img")
+document.querySelector(".hamburg").addEventListener("click", ()=>{
+    if(hamnav.style.right=="-215px"){
+    hamnav.style.right="0";
+    img.src="./assets/cross.svg"
+}
+else{
+    hamnav.style.right="-215px";
+    img.src="./assets/hamburger.svg"
+    }
+})
